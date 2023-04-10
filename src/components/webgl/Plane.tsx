837a28d06beb97aa5a889f2efba05774fe73efa0
KyoheiOrg <tgifz5jmr@gmail.com>
2023-04-10 23:19:35 +0900

    板ポリを独立したコンポーネントに変更

---
+++ src/components/webgl/Plane.tsx
@@ -0,0 +1,44 @@
+import { useTexture } from "@react-three/drei";
+import { useThree, useFrame } from "@react-three/fiber";
+import { FC } from "react";
+
+type ShaderProps = Partial<{
+  vertexShader: string;
+  fragmentShader: string
+}>
+
+export const Plane:FC<ShaderProps> = ({
+  vertexShader,
+  fragmentShader
+}) => {
+  const { clock } = useThree()
+  const texture = useTexture('https://source.unsplash.com/F1tIyqDysbs')
+  
+  const uniform = {
+    u_time:{ value: clock.getElapsedTime() },
+    u_texture: {value: texture}
+  }
+
+  useFrame(({clock})=>{
+    uniform.u_time.value = clock.getElapsedTime()
+  })
+
+
+
+  return (
+    <mesh
+      position={[0, 0, 0]}
+    >
+      <planeGeometry 
+        args={[1.618 * 3, 1 * 3, 16, 16]}
+      />
+      <shaderMaterial
+        attach={"material"}
+        uniforms={ uniform }
+        vertexShader={ vertexShader }
+        fragmentShader={ fragmentShader }
+        uniformsNeedUpdate={ true }
+      />
+    </mesh>
+  )
+}
--- src/pages/wavy.tsx
+++ src/pages/wavy.tsx
@@ -1,42 +1,9 @@
-import { useTexture } from "@react-three/drei";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Canvas} from "@react-three/fiber";
 import { FC } from "react";
 import vertexShader from '/src/shader/wavy.vert?raw'
 import fragmentShader from '/src/shader/wavy.frag?raw'
 import { Container } from '../components/Container';
-
-const Plane:FC = () => {
-  const { clock } = useThree()
-  const texture = useTexture('https://source.unsplash.com/F1tIyqDysbs')
-  
-  const uniform = {
-    u_time:{ value: clock.getElapsedTime() },
-    u_texture: {value: texture}
-  }
-
-  useFrame(({clock})=>{
-    uniform.u_time.value = clock.getElapsedTime()
-  })
-
-
-
-  return (
-    <mesh
-      position={[0, 0, 0]}
-    >
-      <planeGeometry 
-        args={[1.618 * 3, 1 * 3, 16, 16]}
-      />
-      <shaderMaterial
-        attach={"material"}
-        uniforms={ uniform }
-        vertexShader={ vertexShader }
-        fragmentShader={ fragmentShader }
-        uniformsNeedUpdate={ true }
-      />
-    </mesh>
-  )
-}
+import { Plane } from "../components/webgl/Plane";
 
 export const Wavy:FC = () => {
   return (
@@ -44,7 +11,10 @@ export const Wavy:FC = () => {
       <Canvas dpr={[1,2]}>
         <ambientLight intensity={ 0.1 } />
         <directionalLight position={[ 0, 0, 5 ]} />
-        <Plane />
+        <Plane
+          vertexShader={ vertexShader }
+          fragmentShader={ fragmentShader }
+        />
       </Canvas>
     </Container>
   )
