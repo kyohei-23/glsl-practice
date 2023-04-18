a9d9758e729bf823fd61c69d835b6b879c0397c0
KyoheiOrg <tgifz5jmr@gmail.com>
2023-04-18 11:28:25 +0900

    fix error: Invallid hooks call

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { useNavigate, RouterProvider } from 'react-router-dom'
+import { Navigate, RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import './index.css'
-
-const navigate = useNavigate()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -14,7 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
 const notFoundPath = sessionStorage.getItem("targetPath")
 if(notFoundPath){
-  navigate(notFoundPath, {
-    replace: true
-  })
+  <Navigate to={notFoundPath} />
 }
